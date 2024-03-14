6eca81e98e2c180a148ea11e6f7c4c59040f1710
Catherine <ducharme.catherine@tutanota.com>
2024-03-14 09:50:36 -0400

    tested deployment and made adjustments accordingly

--- tgw.tf
+++ tgw.tf
@@ -7,16 +7,19 @@ resource "aws_ec2_transit_gateway" "transit_gateway" {
 resource "aws_ec2_transit_gateway_vpc_attachment" "on_prem_attachment" {
   transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
   vpc_id             = aws_vpc.onprem.id
+  subnet_ids         = ["${aws_subnet.public_subnet_prem.id}"]
 }
 
 # Attach Non-Prod VPC to Transit Gateway
 resource "aws_ec2_transit_gateway_vpc_attachment" "non_prod_attachment" {
   transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
   vpc_id             = aws_vpc.nonprod.id
+  subnet_ids         = ["${aws_subnet.private_subnet_nonprod.id}"]
 }
 
 # Attach Prod VPC to Transit Gateway
-resource "aws_ec2_transit_gateway_vpc_attachment" "prod_attachment" {
-  transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
-  vpc_id             = aws_vpc.prod_vpc.id
-}
+#resource "aws_ec2_transit_gateway_vpc_attachment" "prod_attachment" {
+#  transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
+#  vpc_id             = aws_vpc.prod_vpc.id
+#  subnet_ids         = []
+#}
